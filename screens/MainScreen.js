97e5e405bb314844d48be8284aade8848a906047
ChadoNihi <chadoishere@gmail.com>
2016-11-26 15:28:55 +0200

    working on MainScreen

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Container, Header, InputGroup, Input, Icon, Button } from 'native-base';
 
 import CardSwiper from '../components/CardSwiper.js';
+import HealthChart from '../components/HealthChart.js';
 
 const meals = []; //image, title
 ​
@@ -21,11 +22,17 @@ export const MainScreen = ({onForward, onBack})=> {
 
       <Content>
         <HealthChart />
-        
+
+        <Container>
+          <H3>Your Menu for Today</H3>
+          <Button bordered info> This week </Button>
+        </Container>
         <CardSwiper />
         <CardSwiper />
         <CardSwiper />
       </Content>
+
+      <Footer />
     </Container>
   );
 }
