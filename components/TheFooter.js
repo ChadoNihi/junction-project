ca751991c2539ee3093a6397e4bd99c3121a568c
ChadoNihi <chadoishere@gmail.com>
2016-11-27 08:06:08 +0200

    footer icons

--- components/TheFooter.js
+++ components/TheFooter.js
@@ -7,16 +7,20 @@ export const TheFooter = ()=> {
             <Footer>
                 <FooterTab>
                     <Button active>
+                      <ion-icon name="ios-timer-outline"></ion-icon>
                       Today
                     </Button>
                     <Button>
+                      <ion-icon name="ios-calendar-outline"></ion-icon>
                       This Week
                     </Button>
                     <Button>
                       <Badge>2</Badge>
+                      <ion-icon name="ios-analytics-outline"></ion-icon>
                       Your Health
                     </Button>
                     <Button>
+                      <ion-icon name="md-settings"></ion-icon>
                       Setting
                     </Button>
                 </FooterTab>
